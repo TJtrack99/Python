a12da73fd71946f22a892dd6af7e80ce9436df03
TJtrack99 <timothy.jenson@gmail.com>
2015-02-17 11:29:02 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,5 @@
 print "Blackjack program"
+cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+flag = 1
+while(flag==1):
+  draw = 
