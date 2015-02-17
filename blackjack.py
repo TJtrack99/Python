7884a97d534c068139ed8a26de2a66d2ba04ab0a
TJtrack99 <timothy.jenson@gmail.com>
2015-02-17 11:30:06 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,7 @@
 print "Blackjack program"
 cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+import random
 flag = 1
 while(flag==1):
-  draw = 
+  draw = random.randint(0, 52)-1
+  
